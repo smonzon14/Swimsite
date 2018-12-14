499478e46977df03e199a45f61bd9e55575acd14
Sebastian Monzon <smonzon360@gmail.com>
2018-12-14 09:14:38 -0500

    Clean up css

--- main.js
+++ main.js
@@ -6,24 +6,17 @@ var data = {"SebastianMonzon" : ["12", "4 years", "Freestyle"], "EamonnBurke" : 
             "ThiagoNunes" : ["11", "3 years", "Freestyle"], "EthanProspere" : ["11", "3 years", "Butterfly"],
             "GabeMonzon" : ["11", "3 years", "Freestyle"], "JimFredrick" : ["11", "3 years", "Freestyle"],
             "MilesDelGrecco" : ["11", "3 years", "Breast-stroke"], "CamWhite" : ["11", "2 years", "Freestyle"],
-            "AndersonPhan" : ["10", "2 years", "Freestyle"],}
+            "AndersonPhan" : ["10", "2 years", "Breast-stroke"],}
 var onMouseOver = function() {
     var name = this.textContent;
-    
     name = name.replace(/\s/g, "");
     this.style.backgroundColor = "rgb(184, 16, 31)";
-    //open info
     var memberData = data[name];
-    console.log(name)
     stats.innerHTML = "Grade: " + memberData[0] + "<br>Member for: " + memberData[1] + "<br>Best Stroke: " + memberData[2] + "";
-
 };
 var onMouseOut = function() {
-
     this.style.backgroundColor = "rgba(30,30,30,0.75)";
-    //close info
     stats.textContent = "Hover Over Member For Stats";
-
 };
 for (var i = 0; i < classname.length; i++) {
     classname[i].addEventListener('mouseover', onMouseOver, false);
